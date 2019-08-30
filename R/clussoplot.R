bc36804257670611fd2547298bffadb3122c8283
mkamenet3 <maria.kamenetsky@gmail.com>
2019-08-30 07:36:36 -0500

    added packages for clussoplot, started clussoplot()

---
+++ R/clussoplot.R
@@ -0,0 +1,15 @@
+#'Plot coefficient paths from clusso
+#'
+#'@title
+#'clussoplot
+#'@description Plot the coefficient paths from the LASSO-identified potential clusters.
+#'@param
+#'
+#'
+#'
+#'@export
+#'@return Returns a plot
+#'@examples
+
+
+
