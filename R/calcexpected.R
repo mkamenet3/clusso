204eb653f3e1df71308a2476437ff28baac4f562
mkamenet3 <maria.kamenetsky@gmail.com>
2019-11-24 20:07:26 -0600

    updated calc expected and added test

--- R/calcexpected.R
+++ R/calcexpected.R
@@ -4,10 +4,10 @@
 #'calcexpected
 #' @description
 #' Expected counts can be estimated if not available, following selected distribution function
-#'@param observed Vector of observed counts
-#'@param periods Cector of time periods matching observed counts 
+#'@param observed Vector of observed counts.
+#'@param population vector of population sizes in each geographic unit.
+#'@param periods Vector of time periods matching observed counts. 
 #'@param ids Vector of centroid identifiers matching the observed counts.
-#'@param family Must be provided. Can either be specified as \code{poisson} or \code{binomial}.
 #'@export
 #'@return returns vector of expected counts
 #'@examples
@@ -15,18 +15,15 @@
 #'data(japanbreastcancer)
 #'calcexpected(japanbreastcancer$observed, japanbreastcancer$period, japanbreastcancer$id, family="poisson") }
 
-calcexpected <- function(observed, periods, ids,family=c("poisson","binomial")){
-    if(missing(family)){
-        stop("You must specify family to be either 'poisson' or 'binomial'")
-    }
+calcexpected <- function(observed, population,periods, ids){
     if(is.character(periods)){
         periods <- as.factor(periods)
     }
     if(is.character(ids)){
         ids <- as.factor(ids)
     }
-    family <- match.arg(family, several.ok = FALSE)
-    switch(family, 
-           poisson=fitted(glm(observed ~ ids + periods, family="poisson")),
-           binomial=fitted(glm(observed ~ ids + periods, family="binomial")))
+    rate <- sum(observed)/sum(population)
+    expected <- population*rate
+    dat <- cbind.data.frame(ids, periods, observed, expected)
+    return(dat)
 }
