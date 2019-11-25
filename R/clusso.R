b5e61f088d36226eb208ba1f93615dd8e2f4bfca
mkamenet3 <maria.kamenetsky@gmail.com>
2019-11-25 07:14:34 -0600

    intermediate commit to see what other R scripts I changed and deleted old dbinom so I don't override that function anymore (its now dbin)

--- R/clusso.R
+++ R/clusso.R
@@ -6,11 +6,16 @@
 #'clusso
 #' @description
 #' Runs helper function for both the space and space-time Lasso model on observed data. 
-#'@param clst list; output from \code{toclusso} function. Must be of class \code{clst}.
+#'@param df name of dataframe.
+#'@param expected Name of variable that contains the expected counts.
+#'@param observed Name of variable that contains the observed counts.
+#'@param timeperiod Name of variable that contains the timeperiod in which counts were observed (as factor). 
+#'If spatial-only analysis, create a column that has a single value (ex: "Time1") and convert this to a factor.
+#'@param covars are there additional covariates in the dataframe beyond the three required? If so, set to \code{TRUE}. Default is \code{FALSE}.
+#'@param id Optional. If your dataframe contains an ID variable that should not be a covariate, set the name here.
 #'@param x x coordinates (easting/latitude); if utm coordinates, scale to km.
 #'@param y y coordinates (northing/longitude); if utm coordinates, scale to km.
 #'@param rMax set max radius (in km).
-#'@param Time Number of time periods or years in your dataset. Must be declared as numeric.
 #'@param utm default is \code{TRUE}. If \code{FALSE}, then coordinates will be interpreted as Longitude/Latitude and the haversine formula will be used to determine the distance between points.
 #'@param longdat Is the data in panel/long format? Default is \code{TRUE}. For wide format, specify \code{FALSE} (TODO).
 #'@param analysis A string specifying if the spatial (\code{"space"}), spatio-temporal (\code{"spacetime"}), or both spatial and spatio-temporal (\code{"both"}) analysis should be executed. Default is \code{"both"}. 
@@ -34,12 +39,29 @@
 #'system.time(resreal <- clusso(clst, x,y, rMax, Time, utm=TRUE, analysis="both", maxclust=10))
 #'  }
 
-
-clusso <- function(clst, x,y,rMax, Time, utm=TRUE, longdat=TRUE, analysis = c("space","spacetime", "both"),model = c("poisson", "binomial"),maxclust = 11,overdispfloor=TRUE, cv = NULL, collapsetime=FALSE){
-    if(is(clst, "clst")!=TRUE) stop("clst element not of class `clst`. This is required for the clusso function.")
+# clusso <- function(clst, x,y,rMax, Time, utm=TRUE, longdat=TRUE, analysis = c("space","spacetime", "both"),model = c("poisson", "binomial"),maxclust = 11,overdispfloor=TRUE, cv = NULL, collapsetime=FALSE){
+clusso2 <- function(df, expected, observed, timeperiod,covars,id= NULL,x,y,rMax, utm=TRUE, longdat=TRUE, analysis = c("space","spacetime", "both"),model = c("poisson", "binomial"),maxclust = 11,overdispfloor=TRUE, cv = NULL, collapsetime=FALSE){
+    expected <- eval(substitute(expected),df)
+    observed <- eval(substitute(observed),df)
+    timeperiod <- eval(substitute(timeperiod),df)
+    if(!is.null(id)){
+        id<- eval(substitute(id),df)
+    }
+    else{
+        id <- NULL
+    }
+    if((missing(covars) | covars==FALSE)){
+        covars <- FALSE
+    }
+    else{
+        covars <- TRUE
+    }
+    clst <- toclusso(df, expected, observed,timeperiod, covars, id)
+    #if(is(clst, "clst")!=TRUE) stop("clst element not of class `clst`. This is required for the clusso function.")
     expected <- clst$required_df$expected
     observed <- clst$required_df$observed
     period <- clst$required_df$timeperiod
+    Time <- length(unique(period))
     #initial user setting
     if(!is.null(clst$othercovariates_df)){
         covars <- clst$othercovariates_df
@@ -95,7 +117,7 @@ clusso <- function(clst, x,y,rMax, Time, utm=TRUE, longdat=TRUE, analysis = c("s
     if(length(analysis) > 1) stop("You must select either `space`, `spacetime`, or `both`")
     analysis <- match.arg(analysis, several.ok = FALSE)
     if(model=="poisson"){
-        print("Poisson models initiated")
+        #print("Poisson models initiated")
         switch(analysis, 
                space = clussoPois(analysis="space",x, y, rMax,period, expected, observed, covars, 
                                   Time, utm, longdat, maxclust,overdispfloor, cv, collapsetime),
@@ -105,13 +127,98 @@ clusso <- function(clst, x,y,rMax, Time, utm=TRUE, longdat=TRUE, analysis = c("s
                                  Time, utm, longdat, maxclust,overdispfloor, cv, collapsetime))
     }
     else if(model=="binomial"){
-        print("Binomial models initiated")
+        #print("Binomial models initiated")
         switch(analysis, 
                space = clussoBinom(analysis="space",x, y, rMax,period, expected, observed, covars,
                                    Time, utm, longdat, maxclust, cv, collapsetime),
                spacetime = clussoBinom(analysis="spacetime",x, y, rMax,period, expected, observed, covars,
                                        Time, utm, longdat, maxclust, cv, collapsetime),
                both = clussoBinom(analysis="both",x, y, rMax,period, expected, observed, covars, 
+                                  Time, utm, longdat, maxclust, cv, collapsetime))
+    }
+    else{
+        warning("You have not specified a model. Please set 'model' argument to 'poisson' or 'binomial'.")
+    }
+    
+    }
+
+clusso <- function(clst, x,y,rMax, Time, utm=TRUE, longdat=TRUE, analysis = c("space","spacetime", "both"),model = c("poisson", "binomial"),maxclust = 11,overdispfloor=TRUE, cv = NULL, collapsetime=FALSE){
+    if(is(clst, "clst")!=TRUE) stop("clst element not of class `clst`. This is required for the clusso function.")
+    expected <- clst$required_df$expected
+    observed <- clst$required_df$observed
+    period <- clst$required_df$timeperiod
+    #initial user setting
+    if(!is.null(clst$othercovariates_df)){
+        covars <- clst$othercovariates_df
+    }
+    else{
+        covars <- NULL
+    }
+
+    if((missing(utm) | utm==TRUE)){
+        utm=TRUE
+    }
+    else{
+        message("Coordinates are assumed to be in lat/long coordinates. For UTM coordinates, please specify 'utm=TRUE' or leave empty for default (TRUE).")
+        utm=FALSE
+    }
+    if((missing(longdat) | longdat==TRUE)){
+        longdat=TRUE
+    }
+    else{
+        longdat=FALSE
+        #message("Data assumed to be in panel data. To use vector data instead, please specify 'longdat=FALSE'")
+    }
+    if(missing(maxclust)){
+        maxclust = 11 + Time
+        #print(maxclust)
+    }
+    else{
+        maxclust = maxclust + Time
+        #print(maxclust)
+    }
+    if((missing(overdispfloor) | overdispfloor==TRUE)){
+        overdispfloor <- TRUE
+    }
+    else{
+        overdispfloor <- FALSE
+    }
+    if(!is.null(cv)){
+        message("Running cross-validation method for path selection. For AIC, AICc, and BIC, set `cv = NULL`")
+        cv = cv
+    }
+    else{
+        cv = NULL
+    }
+    if(collapsetime==TRUE){
+        warning("You've selected to collapse time periods on space. This will results in averaging of counts by geographic unit.
+                To allow time to vary across geographic unit, select `collapsetime=FALSE` (DEFAULT).")
+    }
+    else{
+        collapsetime=FALSE
+    }
+    if(length(model) > 1) stop("You must select either `poisson` or `binomial`")
+    model <- match.arg(model)
+    if(length(analysis) > 1) stop("You must select either `space`, `spacetime`, or `both`")
+    analysis <- match.arg(analysis, several.ok = FALSE)
+    if(model=="poisson"){
+        print("Poisson models initiated")
+        switch(analysis,
+               space = clussoPois(analysis="space",x, y, rMax,period, expected, observed, covars,
+                                  Time, utm, longdat, maxclust,overdispfloor, cv, collapsetime),
+               spacetime = clussoPois(analysis="spacetime",x, y, rMax,period, expected, observed, covars,
+                                      Time, utm, longdat, maxclust,overdispfloor, cv, collapsetime),
+               both = clussoPois(analysis="both",x, y, rMax,period, expected, observed, covars,
+                                 Time, utm, longdat, maxclust,overdispfloor, cv, collapsetime))
+    }
+    else if(model=="binomial"){
+        print("Binomial models initiated")
+        switch(analysis,
+               space = clussoBinom(analysis="space",x, y, rMax,period, expected, observed, covars,
+                                   Time, utm, longdat, maxclust, cv, collapsetime),
+               spacetime = clussoBinom(analysis="spacetime",x, y, rMax,period, expected, observed, covars,
+                                       Time, utm, longdat, maxclust, cv, collapsetime),
+               both = clussoBinom(analysis="both",x, y, rMax,period, expected, observed, covars,
                                   Time, utm, longdat, maxclust, cv, collapsetime))
     }
     else{
