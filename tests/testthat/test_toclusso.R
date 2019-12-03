74fe30022f8167900f7f5e8b9a8e1b1a3c2defb2
mkamenet3 <maria.kamenetsky@gmail.com>
2019-12-03 13:23:10 -0600

    updated test to reflect new use of toclusso inside clusso main function

--- tests/testthat/test_toclusso.R
+++ tests/testthat/test_toclusso.R
@@ -7,15 +7,20 @@ context("Testing toclusso")
 test_that("Length of inputs the same",{
     set.seed(2)
     period <- rep(seq(1,2),5)
+    periodF <- as.factor(period)
     expected <- rnegbin(n = 10,mu = 15,theta = 1000)
     observed <- rnegbin(expected, theta=1000)
+    covars <- FALSE
+    id <- NULL
+    requiredcolNames <- c("expected", "observed", "period")
+    requiredcolNamesF <- c("expected", "observed", "periodF")
     Time = 2
-    dat <- cbind.data.frame(expected, observed, period)
+    dat <- cbind.data.frame(expected, observed, period, periodF)
     
-    expect_warning(toclusso(dat,expected, observed, period))
-    expect_error(toclusso(dat,expected, observed[-1], as.factor(period)))
-    expect_error(toclusso(c(expected,9), observed, as.factor(period)))
-    expect_error(toclusso(C(expected,9), observed[-1], as.factor(period)))
+    expect_warning(toclusso(dat,expected, observed, period, covars, id, requiredcolNames))
+    expect_error(toclusso(dat,expected, observed[-1], periodF, covars, id, requiredcolNamesF))
+    expect_error(toclusso(c(expected,9), observed, periodF, covars, id, requiredcolNamesF))
+    expect_error(toclusso(C(expected,9), observed[-1], periodF, covars, id, requiredcolNamesF))
 })
 
 
