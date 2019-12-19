9e73f565832fc6315e339c0e09cfb8733b47957a
mkamenet3 <maria.kamenetsky@gmail.com>
2019-12-18 21:33:55 -0600

    added dbern()

---
+++ R/dbern.R
@@ -0,0 +1,18 @@
+#' Bernoullii Function
+#' @title dbern
+#' @param Yx Cases (0 or 1).
+#' @param Ex Total number of trials (1's).
+#' @param p Vector of estimated probabilities for each individual.
+#' @return Returns vector of Bernoulli log-likelihood for each proposed path of the LASSO tuning parameter values.
+#' @example 
+dbern <- function(Yx, Ex,p){
+    sum(dbinom(Yx, size=1, p,log=TRUE))
+}
+#' Convert linear predictor to probability scale
+#' @title pihat
+#' @param xbeta Sparse matrix of potential space-time clusters times coefficient for each lasso path.
+#' @return Returns matrix of estimated probabilities for each space-time location by each lasso path.
+pihat <- function(xbeta){
+    phat <- 1/(1+exp(-xbeta))
+    return(phat)
+}
